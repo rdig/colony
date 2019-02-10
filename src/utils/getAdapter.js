1333fd38b42d5a69afab3633dddf4600a8e46341
Raul Glogovetan <raul@glogovetan.com>
2019-02-10 23:24:20 +0200

    Remove: software wallet and fake it in the adapter

--- src/utils/getAdapter.js
+++ src/utils/getAdapter.js
@@ -2,7 +2,6 @@ import EthersAdapter from '@colony/colony-js-adapter-ethers';
 
 import getProvider from './getProvider';
 import getNetworkLoader from './getNetworkLoader';
-import getSoftwareWallet from './getSoftwareWallet';
 
 import { RINKEBY } from '~const';
 
@@ -10,7 +9,7 @@ const getAdapter = (network = RINKEBY) => {
   const provider = getProvider(network);
   return new EthersAdapter({
     loader: getNetworkLoader(network),
-    wallet: getSoftwareWallet(provider),
+    wallet: { provider },
     provider,
   });
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 export { default as getNetworkLoader } from './getNetworkLoader';
 export { default as getProvider } from './getProvider';
-export { default as getSoftwareWallet } from './getSoftwareWallet';
 export { default as getAdapter } from './getAdapter';
 
 export { default as asyncCall } from './asyncCall';
