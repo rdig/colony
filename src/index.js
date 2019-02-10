c62f0c83bd61f93f1430b884ea795d282cd419bd
Raul Glogovetan <raul@glogovetan.com>
2019-02-10 19:06:22 +0200

    Add: network context, list action

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import networkClient from './networkClient';
+import chalk from 'chalk';
 
-import { asyncCall } from '~utils';
+import { asyncCall, getQuery } from '~utils';
+import { CONTEXT, ACTION } from '~const';
 
 const mainProcess = async () => {
+  const { context, action } = getQuery();
   const networkClientInstance = await networkClient();
-  const coloniesCount = await asyncCall(
-    networkClientInstance.getColonyCount,
-    'Loading Colonies',
-  );
-  console.log(coloniesCount);
+  switch (context) {
+  case CONTEXT.NETWORK: {
+    const { count: coloniesCount } = await asyncCall(
+      networkClientInstance.getColonyCount,
+      'Loading Colonies',
+    );
+    switch (action) {
+    case ACTION.LIST[0]: {
+      for (let i = 1; i <= coloniesCount; i += 1) {
+        const { address: currentColonyAddress } = await asyncCall(
+          () => networkClientInstance.getColony.call({ id: i }),
+          `Loading Colony Id ${chalk.dim(i)}`,
+        );
+        const { version: currentColonyVersion } = await asyncCall(
+          networkClientInstance.getCurrentColonyVersion,
+          `Loading Colony Id ${chalk.dim(i)}`,
+        );
+        console.log(
+          chalk.green('id'),
+          i,
+          chalk.green('address'),
+          `${chalk.dim('0x')}${currentColonyAddress.slice(2)}`,
+          chalk.green('v'),
+          currentColonyVersion,
+        );
+      }
+      return null;
+    }
+    default: {
+      return console.log(chalk.red('No <action> output! Something went terribly wrong...'));
+    }
+    }
+  }
+  default: {
+    return console.log(chalk.red('No <context> output! Something went terribly wrong...'));
+  }
+  }
 };
 
 mainProcess().then(process.exit);
