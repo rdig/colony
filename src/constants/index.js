2b5d08f11d71c93d55735f6fff2c92caf7a47a87
Raul Glogovetan <raul@glogovetan.com>
2019-02-10 18:20:50 +0200

    Fix: `getQuery` cli util to display help

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,18 +1,59 @@
 export const RINKEBY = 'rinkeby';
 
 export const CONTEXT = {
-  COLONY: 'colony',
   NETWORK: 'network',
-  TOKEN: 'token',
+  /*
+   * @TODO To be implemented
+   */
+  // COLONY: 'colony',
+  // TOKEN: 'token',
+};
+
+export const CONTEXT_HELP = {
+  NETWORK: 'Execute actions in the context of a specific Colony',
+  /*
+   * @TODO To be implemented
+   */
+  // COLONY: 'Execute actions in the context of the Colony Network',
+  // TOKEN: 'Execute actions in the context of a specific Token',
 };
 
 export const ACTION = {
   LIST: ['list', 'ls', 'l'],
 };
 
+export const ACTION_HELP = {
+  LIST: 'List action, in the currently selected context',
+};
+
 /*
  * Process exit codes
  */
 export const STATUS = {
-  CANNOT_EXECUTE: 127,
+  CANNOT_EXECUTE: 126,
+  NOT_FOUND: 127,
+};
+
+export const TABLE_OPTS = {
+  chars: {
+    top: '',
+    'top-mid': '',
+    'top-left': '',
+    'top-right': '',
+    bottom: '',
+    'bottom-mid': '',
+    'bottom-left': '',
+    'bottom-right': '',
+    left: '',
+    'left-mid': '',
+    mid: '',
+    'mid-mid': '',
+    right: '',
+    'right-mid': '',
+    middle: '          ',
+  },
+  style: {
+    'padding-left': 0,
+    'padding-right': 0,
+  },
 };
--- src/utils/cli.js
+++ src/utils/cli.js
@@ -3,13 +3,14 @@ import chalk from 'chalk';
 import flattenDeep from 'lodash.flattendeep';
 
 import { CONTEXT, ACTION, STATUS } from '~const';
+import getHelp from './getHelp';
 
 const { _: query, ...options } = minimist(process.argv.slice(2));
 
 const validateContext = (currentContext) => {
   const contextIndex = flattenDeep(Object.values(CONTEXT))
     .findIndex(context => context === currentContext);
-  if (contextIndex >= 0) {
+  if (currentContext === undefined || contextIndex >= 0) {
     return currentContext;
   }
   console.log(chalk.red(`Specified context is not available: "${currentContext}"`));
@@ -19,20 +20,36 @@ const validateContext = (currentContext) => {
 const validateAction = (currentAction) => {
   const actionIndex = flattenDeep(Object.values(ACTION))
     .findIndex(action => action === currentAction);
-  if (actionIndex >= 0) {
+  if (currentAction === undefined || actionIndex >= 0) {
     return currentAction;
   }
   console.log(chalk.red(`Specified action is not available: "${currentAction}"`));
   return process.exit(STATUS.CANNOT_EXECUTE);
 };
 
-export const getQuery = () => ({
-  context: validateContext(query[0]),
-  action: validateAction(query[1]),
-  /*
-   * @TODO Validate query selector (eg: Id, Id range, ensName)
-   */
-  selector: query[2],
-});
+export const getQuery = () => {
+  const context = validateContext(query[0]);
+  const action = validateAction(query[1]);
+  if (!context) {
+    console.log(`${chalk.yellow('You need to select the <context> in which to run')}
+`);
+    getHelp();
+    return process.exit(STATUS.NOT_FOUND);
+  }
+  if (!action) {
+    console.log(`${chalk.yellow('You need to select the <action> which should run in the <context>')}
+`);
+    getHelp();
+    return process.exit(STATUS.NOT_FOUND);
+  }
+  return {
+    context,
+    action,
+    /*
+     * @TODO Validate query selector (eg: Id, Id range, ensName)
+     */
+    selector: query[2],
+  };
+};
 
 export const getOptions = () => options;
